814014fba277f3540e58b007525e5cc0ae10e6c2
Matthias Klees <szenekonzept@googlemail.com>
2015-05-20 21:11:54 +0200

    geändert:               src/qt/addressbookpage.cpp 	geändert:               src/qt/bitcoingui.cpp 	geändert:               src/qt/coincontroldialog.cpp 	geändert:               src/qt/guiutil.cpp 	geändert:               src/qt/overviewpage.cpp 	geändert:               src/qt/sendcoinsdialog.cpp 	geändert:               src/qt/transactionview.cpp

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -11,6 +11,9 @@
 #include <QFont>
 #include <QLineEdit>
 #include <QUrl>
+#if QT_VERSION >= 0x050000
+#include <QUrlQuery>
+#endif
 #include <QTextDocument> // For Qt::escape
 #include <QAbstractItemView>
 #include <QApplication>
@@ -84,7 +87,12 @@ bool parseBitcoinURI(const QUrl &uri, SendCoinsRecipient *out)
     SendCoinsRecipient rv;
     rv.address = uri.path();
     rv.amount = 0;
+    #if QT_VERSION >= 0x050000    
+		QUrlQuery qu(uri);
+    QList<QPair<QString, QString> > items = qu.queryItems();
+#else
     QList<QPair<QString, QString> > items = uri.queryItems();
+#endif
     for (QList<QPair<QString, QString> >::iterator i = items.begin(); i != items.end(); i++)
     {
         bool fShouldReturnFalse = false;
@@ -137,7 +145,11 @@ bool parseBitcoinURI(QString uri, SendCoinsRecipient *out)
 
 QString HtmlEscape(const QString& str, bool fMultiLine)
 {
+  #if QT_VERSION >= 0x050000
+    QString escaped = str.toHtmlEscaped();
+#else
     QString escaped = Qt::escape(str);
+#endif
     if(fMultiLine)
     {
         escaped = escaped.replace("\n", "<br>\n");
@@ -172,7 +184,11 @@ QString getSaveFileName(QWidget *parent, const QString &caption,
     QString myDir;
     if(dir.isEmpty()) // Default to user documents location
     {
+     #if QT_VERSION >= 0x050000
+        myDir = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
+#else
         myDir = QDesktopServices::storageLocation(QDesktopServices::DocumentsLocation);
+#endif
     }
     else
     {
